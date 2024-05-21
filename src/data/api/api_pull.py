109bd73cf12cd56ffc396fd78f9b582bf7cbb9ef
Graham Greene <123007914+grahamgreene88@users.noreply.github.com>
2024-05-20 22:30:44 -0400

    Added code for API pull to data source code.

---
+++ src/data/api/api_pull.py
@@ -0,0 +1,58 @@
+import requests
+import json
+import pandas as pd
+import logging
+from requests import HTTPError, Timeout, RequestException
+from datetime import datetime, timedelta
+from urllib.parse import quote
+
+
+def pull(API_KEY):
+    """This function pulls data from the OpenAQ API to obtain the pollutant, value, update time, and unit
+    for the last seven days.
+    
+    
+    Args:
+        API_KEY from .env file.
+
+    Returns:
+        Dataframe with 4 columns and 2 rows. One row for each pollutant.
+        
+    Raises:
+        HTTPError, Timeout and RequestException
+    """
+
+    # Get datetimes for present and one week ago
+    current_datetime = datetime.utcnow()
+    week_ago_datetime = current_datetime - timedelta(days=7)
+    # Format datetimes to ISO 8601
+    date_from = week_ago_datetime.isoformat()
+    date_to = current_datetime.isoformat()
+   
+    # Insert date_from and date_to into API url
+    url = f"https://api.openaq.org/v2/measurements?date_from={quote(date_from)}&date_to={quote(date_to)}&limit=1000&page=1&offset=0&sort=desc&has_geo=true&parameter=o3&parameter=pm25&parameter=no2&location_id=1275379&order_by=datetime"
+
+    headers = {
+        "X-API-Key": API_KEY,
+        "accept": "application/json"}
+
+    session = requests.Session()
+    session.headers.update(headers)
+
+    # try-except blocks for robust error handling and logging
+    try:
+        response = session.get(url, headers=headers, timeout=15)
+        response.raise_for_status()
+    
+    except HTTPError as http_err:
+        logger.error(f"HTTP error occurred: {http_err}")
+        
+    except Timeout:
+        logger.error("Request timed out after 15 seconds.")
+        
+    except RequestException as request_err:
+        logger.error("Request error occurred: {request_err}")
+
+    aq_data = json.loads(response.text)
+    aq_df = pd.json_normalize(aq_data['results'])
+    return aq_df
---
+++ src/data/api/clean_pull.py
@@ -0,0 +1,45 @@
+import pandas as pd
+
+
+def clean_df(df):
+    """cleans air quality dataframe into desired format.
+
+    Args:
+        df (dataframe): AQ dataframe pulled from API.
+    """
+    # Select desired columns
+    small_df = df.loc[:, ['date.local', 'parameter', 'value', 'unit', 'location', 
+                          'coordinates.latitude', 'coordinates.longitude']]
+    # Sort by date descending
+    small_df.sort_values('date.local', ascending=False, inplace=True)
+    # Convert ppm units to ppb
+    subset = (small_df['parameter'] == 'o3') | (small_df['parameter'] == 'no2')
+    small_df.loc[subset, 'value'] *= 1000
+    small_df.loc[subset, 'unit'] = 'ppb'
+    # Replace pollutant names with full written names
+    small_df['parameter'] = small_df['parameter'].replace({'pm25': 'Particulate matter (PM2.5)', 'no2': 'Nitrogen dioxide (NO2)', 'o3': 'Ozone (O3)'})
+    # Changing column name to datetime
+    small_df = small_df.rename(columns={'date.local': 'datetime'})
+    # Changing datetime column to datetime data type
+    small_df['datetime'] = pd.to_datetime(small_df['datetime'])
+    return small_df
+    
+    
+    
+    
+    # small_df = df.loc[:, ['date.local', 'parameter', 'value', 'unit']]
+    # pivot_df = small_df.pivot(index='date.local', columns='parameter', values='value')
+    # pivot_df.reset_index()
+    # pivot_df = pivot_df.rename_axis(None, axis=1)
+    # # Inserting column for pollutant units, location, latitude, longitude
+    # pivot_df.insert(1, 'no2_unit', 'ppm')
+    # pivot_df.insert(3, 'o3_unit', 'ppm')
+    # pivot_df.insert(5, 'pm25_unit', 'µg/m³')
+    # pivot_df['location'] = 'Downtown Ottawa'
+    # pivot_df[['coordinates.latitude', 'coordinates.longitude']] = (45.434333, -75.67599)
+    # final_df = pivot_df.reset_index()
+    # # Changing lastupdated column name to date
+    # final_df.rename(columns={'date.local': 'datetime'}, inplace=True)
+    # # Changing date column to datetime data type
+    # final_df['datetime'] = pd.to_datetime(final_df['datetime'])
+    # final_df.sort_values('datetime', ascending=False, inplace=True)
