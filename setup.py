7464258c989d5fe3922238a5b0b5397abf675afc
Graham Greene <123007914+grahamgreene88@users.noreply.github.com>
2024-05-20 21:46:02 -0400

    First commit of air quality project.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project  is an end-to-end data analytics project. that monitors aiqr quality data for the Ottawa region.',
+    author='Graham Greene',
+    license='MIT',
+)
